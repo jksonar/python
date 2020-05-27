d757818c3823cc1fe967c93deacbe1ac4d11b5be
jay <jksonar1992@gmail.com>
2020-05-28 00:57:33 +0530

    search google

---
+++ googleSearch.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+print("sample test case started")
+# driver = webdriver.Chrome()
+driver = webdriver.Firefox()
+# driver=webdriver.ie()
+# maximize the window size
+driver.maximize_window()
+# navigate to the url
+driver.get("https://www.google.com/")
+# identify the Google search text box and enter the value
+driver.find_element_by_name("q").send_keys("jksonar")
+time.sleep(3)
+# click on the Google search button
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+time.sleep(3)
+# close the browser
+driver.close()
+print("sample test case successfully completed")
